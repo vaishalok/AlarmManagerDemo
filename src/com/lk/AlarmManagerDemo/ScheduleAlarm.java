2f75df6dcd47fda099c25f145a6ccfca5787571b
vaishalok <alok@vinsol.com>
2010-08-14 19:53:58 +0530

    Initial commit

---
+++ src/com/lk/AlarmManagerDemo/ScheduleAlarm.java
@@ -0,0 +1,13 @@
+package com.lk.AlarmManagerDemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScheduleAlarm extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
